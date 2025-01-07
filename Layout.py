33d5f8be90b9297a16fa48f79974e2ffef1f5330
Link1515 <h26281660@gmail.com>
2025-01-07 15:27:04 +0800

    feat: align text for different font sizes

--- Layout.py
+++ Layout.py
@@ -13,9 +13,12 @@ class Layout:
         self.weight = "normal"
         self.style = "roman"
         self.size = 12
+        self.line = []
 
         for tok in tokens:
             self.token(tok)
+
+        self.flush()
 
     def token(self, tok):
         if isinstance(tok, Text):
@@ -37,6 +40,11 @@ class Layout:
             self.size += 4
         elif tok.tag == "/big":
             self.size -= 4
+        elif tok.tag == "br":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += Layout.VSTEP
 
     def word(self, word):
         font = tkinter.font.Font(
@@ -47,8 +55,25 @@ class Layout:
         w = font.measure(word)
 
         if self.cursor_x + w >= self.width - Layout.HSTEP:
-            self.cursor_y += font.metrics("linespace") * 1.25
-            self.cursor_x = Layout.HSTEP
+            self.flush()
 
-        self.display_list.append((self.cursor_x, self.cursor_y, word, font))
+        self.line.append((self.cursor_x, word, font))
         self.cursor_x += w + font.measure(" ")
+
+    def flush(self):
+        if not self.line:
+            return
+
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metrics["ascent"] for metrics in metrics])
+
+        baseline = self.cursor_y + 1.25 * max_ascent
+
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+
+        max_descent = max([metrics["descent"] for metrics in metrics])
+        self.cursor_y = baseline + 1.25 * max_descent
+        self.cursor_x = Layout.HSTEP
+        self.line = []
